66d529d3b90d9efbc19e6f1a40b42e88a2afef03
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2023-03-01 15:09:50 -0600

    Use swift package generate flow in Xcode

---
+++ Library/Sources/LibraryObjC/LibraryObjC.m
@@ -0,0 +1,14 @@
+//
+//  LibraryObjC.m
+//  
+//
+//  Created by Samantha Cannillo on 3/1/23.
+//
+
+#import "LibraryObjC.h"
+
+@implementation LibraryObjC
+
+- (NSString *)text;
+
+@end
---
+++ Library/Sources/LibraryObjC/Public/LibraryObjC/LibraryObjC.h
@@ -0,0 +1,20 @@
+//
+//  LibraryObjC.h
+//  
+//
+//  Created by Samantha Cannillo on 3/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LibraryObjC : NSObject
+
+- (NSString *)text {
+    return @"Hello, ObjC!";
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
