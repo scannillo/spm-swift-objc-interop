df1eed8fe82f0172013b8769e689ce617fbe588a
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2023-03-01 14:51:09 -0600

    Add ObjC target

---
+++ Library/LibraryObjC/LibraryObjC.h
@@ -0,0 +1,18 @@
+//
+//  LibraryObjC.h
+//  LibraryObjC
+//
+//  Created by Samantha Cannillo on 3/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibraryObjC.
+FOUNDATION_EXPORT double LibraryObjCVersionNumber;
+
+//! Project version string for LibraryObjC.
+FOUNDATION_EXPORT const unsigned char LibraryObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryObjC/PublicHeader.h>
+
+
---
+++ Library/LibraryObjC/LibrarySourceObjC.h
@@ -0,0 +1,18 @@
+//
+//  LibrarySourceObjC.h
+//  LibraryObjC
+//
+//  Created by Samantha Cannillo on 3/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LibrarySourceObjC : NSObject
+
+- (NSString *)testString;
+
+@end
+
+NS_ASSUME_NONNULL_END
